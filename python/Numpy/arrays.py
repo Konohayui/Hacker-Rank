a46f38607815c7d0953cb485f2475c282f5005f2
shenmemingzine <rongwingyu@gmail.com>
2018-09-05 13:23:49 -0700

    Create arrays.py

---
+++ python/Numpy/arrays.py
@@ -0,0 +1,11 @@
+import numpy
+
+def arrays(arr):
+    # complete this function
+    # use numpy.array
+    arr = arr[::-1]
+    return numpy.array(arr, float)
+    
+arr = raw_input().strip().split(' ')
+result = arrays(arr)
+print(result)
