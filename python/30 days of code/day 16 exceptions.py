8ca0abb88283b708d57ddc36680847ba69b45288
shenmemingzine <rongwingyu@gmail.com>
2018-09-11 11:48:51 -0700

    Create day 16 exceptions.py

---
+++ python/30 days of code/day 16 exceptions.py
@@ -0,0 +1,28 @@
+#!/bin/python3
+
+import sys
+
+S = input().strip()
+
+try:
+    print(int(S))
+except ValueError:
+    print("Bad String")
+
+
+
+'''
+Fail
+'''
+
+#!/bin/python3
+
+import sys
+
+S = input().strip()
+
+try:
+    
+    print(is_int)
+except ValueError:
+    print("Bad String")
