8b6e7a26500098082a068c139f00d087a05b834f
shenmemingzine <rongwingyu@gmail.com>
2018-11-08 15:36:22 -0800

    Create Camel Case.py

---
+++ python/Algorithms/Camel Case.py
@@ -0,0 +1,30 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the camelcase function below.
+def camelcase(s):
+    if len(s) == 0:
+        return 0
+
+    else:
+        words = 1
+        for i in s:
+            if i.isupper():
+                words += 1
+        return words
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = camelcase(s)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
